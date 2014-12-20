theforeman/foreman_docker

commit c57dae5641a637761fb51058ad2607c3a9db8ce1

Fixes #8632 - Move docker tables out of foreman-docker

app/assets/javascripts/foreman_docker/image_step.js
@@ -10,23 +10,23 @@ $(document).ready(function() {
   });
 
   var target = $('#search');
-  //autoCompleteImage(target);
+  //autoCompleteRepo(target);
   target.autocomplete({
-    source: function( request, response ) { autoCompleteImage(target); },
+    source: function( request, response ) { autoCompleteRepo(target); },
     delay: 500,
     minLength: 1
   });
 
   $('#hub_tab').click( function() {
-      $('#image_registry_id').val('');
+      $('#repository_registry_id').val('');
   });
 });
 
-function autoCompleteImage(item) {
+function autoCompleteRepo(item) {
   $.ajax({
     type:'get',
     url: $(item).attr('data-url'),
-    data: { search: item.val(), registry_id: $('#image_registry_id').val() },
+    data: { search: item.val(), registry_id: $('#registry_id').val() },
     //data:'search=' + item.val(),
     success:function (result) {
       if(result == 'true'){
@@ -52,7 +52,7 @@ function setAutocompleteTags() {
   tag.addClass('tags-autocomplete-loading');
   tag.val('');
   var source = [];
-  $.getJSON( tag.data("url"), { search: $('#search').val(), registry_id: $('#image_registry_id').val() },
+  $.getJSON( tag.data("url"), { search: $('#search').val(), registry_id: $('#repository_registry_id').val() },
       function(data) {
         $('#searching_spinner').hide();
         tag.removeClass('tags-autocomplete-loading');
@@ -64,17 +64,17 @@ function setAutocompleteTags() {
   tag.autocomplete('option', 'source', source);
 }
 
-function searchImage(item) {
+function searchRepo(item) {
   setWaitingText('<strong>Searching</strong> in the hub, this can be slow, please wait...');
-  $('#image_search_results').html('');
-  $('#image_search_results').show();
+  $('#repository_search_results').html('');
+  $('#repository_search_results').show();
   $.ajax({
     type:'get',
     dataType:'text',
     url: $(item).attr('data-url'),
-    data: { search: $('#search').val(), registry_id: $('#image_registry_id').val() },
+    data: { search: $('#search').val(), registry_id: $('#repository_registry_id').val() },
     success: function (result) {
-      $('#image_search_results').html(result);
+      $('#repository_search_results').html(result);
     },
     complete: function (result) {
       $('#searching_spinner').hide();
@@ -82,8 +82,8 @@ function searchImage(item) {
   });
 }
 
-function imageSelected(item) {
-  $('#image_search_results').hide();
+function repoSelected(item) {
+  $('#repository_search_results').hide();
   setWaitingText('Image selected: <strong>' + item.text + '</strong>. Retrieving available tags, please wait...');
   $('#search').val(item.text);
   setAutocompleteTags(item);

app/controllers/containers/steps_controller.rb
@@ -19,11 +19,7 @@ module Containers
       when :preliminary
         @container.update_attribute(:compute_resource_id, params[:container][:compute_resource_id])
       when :image
-        image = update_or_create_image(params[:image][:image_id], params[:image][:registry_id])
-        @container.update_attributes!(
-            :image => image,
-            :tag => DockerTag.find_or_create_by_tag_and_docker_image_id!(params[:container][:tag],
-                                                                         image.id))
+        @container.update_attributes!(params[:container])
       when :configuration
         @container.update_attributes(params[:container])
       when :environment
@@ -39,18 +35,6 @@ module Containers
     end
 
     private
-
-    def update_or_create_image(id, registry_id)
-      image = DockerImage.find_or_create_by_image_id!(id)
-      begin
-        image.registries << DockerRegistry.find(registry_id) \
-          unless params[:image][:registry_id].empty?
-      # rubocop:disable Lint/HandleExceptions
-      rescue ActiveRecord::StatementInvalid
-        # ignore, someone else already added the image to the registry
-      end
-      image
-    end
 
     def finish_wizard_path
       container_path(:id => params[:container_id])

app/controllers/containers_controller.rb
@@ -1,9 +1,11 @@
 # rubocop:disable Metrics/ClassLength
 class ContainersController < ::ApplicationController
-  before_filter :find_container, :only => [:show, :auto_complete_image, :auto_complete_image_tags,
-                                           :search_image, :commit]
-  before_filter :find_registry, :only => [:auto_complete_image, :auto_complete_image_tags,
-                                          :search_image]
+  before_filter :find_container, :only => [:show, :auto_complete_repository_name,
+                                           :auto_complete_tag,
+                                           :search_repository, :commit]
+  before_filter :find_registry, :only => [:auto_complete_repository_name,
+                                          :auto_complete_tag,
+                                          :search_repository]
 
   def index
     @container_resources = allowed_resources.select { |cr| cr.provider == 'Docker' }
@@ -36,28 +38,28 @@ class ContainersController < ::ApplicationController
   def show
   end
 
-  def auto_complete_image
+  def auto_complete_repository_name
     exist = if @registry.nil?
               @container.compute_resource.exist?(params[:search])
             else
-              registry_auto_complete_image(params[:search])
+              registry_auto_complete_repository(params[:search])
             end
     render :text => exist.to_s
   end
 
-  def registry_auto_complete_image(term)
+  def registry_auto_complete_repository(term)
     result = ::Service::RegistryApi.new(:url => @registry.url).search(term)
     registry_name = term.split('/').size > 1 ? term :
         'library/' + term
     result['results'].any? { |r| r['name'] == registry_name }
   end
 
-  def auto_complete_image_tags
+  def auto_complete_tag
     # This is the format jQuery UI autocomplete expects
     tags = if @registry.nil?
              @container.compute_resource.tags(params[:search])
            else
-             registry_auto_complete_image_tags(params[:search])
+             registry_auto_complete_tags(params[:search])
            end
     respond_to do |format|
       format.js do
@@ -67,7 +69,7 @@ class ContainersController < ::ApplicationController
     end
   end
 
-  def registry_auto_complete_image_tags(term)
+  def registry_auto_complete_tags(term)
     ::Service::RegistryApi.new(:url => @registry.url).list_repository_tags(term).keys
   end
 
@@ -85,15 +87,18 @@ class ContainersController < ::ApplicationController
                                                     { :container => @container, :e => e }
   end
 
-  def search_image
-    images = if @registry.nil?
-               @container.compute_resource.search(params[:search])
-             else
-               r = ::Service::RegistryApi.new(:url => @registry.url).search(params[:search])
-               r['results']
-             end
+  def search_repository
+    repositories = if @registry.nil?
+                     @container.compute_resource.search(params[:search])
+                   else
+                     r = ::Service::RegistryApi.new(:url => @registry.url)
+                       .search(params[:search])
+                     r['results']
+                   end
     respond_to do |format|
-      format.js { render :partial => 'image_search_results', :locals => { :images => images } }
+      format.js do
+        render :partial => 'repository_search_results', :locals => { :repositories => repositories }
+      end
     end
   end
 
@@ -101,7 +106,7 @@ class ContainersController < ::ApplicationController
 
   def action_permission
     case params[:action]
-    when 'auto_complete_image', 'auto_complete_image_tags', 'search_image'
+    when 'auto_complete_repository_name', 'auto_complete_tag', 'search_repository'
       :view
     when 'commit'
       :commit

app/helpers/container_steps_helper.rb
@@ -10,13 +10,13 @@ module ContainerStepsHelper
   end
 
   def select_registry(f)
-    field(f, 'image[registry_id]', :label => _("Registry")) do
-      collection_select :image, :registry_id,
+    field(f, 'container[registry_id]', :label => _("Registry")) do
+      collection_select :container, :registry_id,
                         DockerRegistry.with_taxonomy_scope_override(@location, @organization)
                           .authorized(:view_registries),
                         :id, :name,
                         { :prompt => _("Select a registry") },
-                        :class => "form-control", :disabled => f.object.image.present?
+                        :class => "form-control", :disabled => f.object.repository_name.present?
     end
   end
 end

app/models/container.rb
@@ -2,21 +2,20 @@ class Container < ActiveRecord::Base
   include Authorizable
 
   belongs_to :compute_resource
-  belongs_to :image, :class_name => 'DockerImage', :foreign_key => 'docker_image_id'
-  belongs_to :tag,   :class_name => 'DockerTag',   :foreign_key => 'docker_tag_id'
+  belongs_to :registry, :class_name => "DockerRegistry", :foreign_key => :registry_id
   has_many :environment_variables, :dependent  => :destroy, :foreign_key => :reference_id,
                                    :inverse_of => :container,
                                    :class_name => 'EnvironmentVariable'
   accepts_nested_attributes_for :environment_variables, :allow_destroy => true
   include ForemanDocker::ParameterValidators
 
-  attr_accessible :command, :image, :name, :compute_resource_id, :entrypoint,
-                  :cpu_set, :cpu_shares, :memory, :tty, :attach_stdin,
+  attr_accessible :command, :repository_name, :name, :compute_resource_id, :entrypoint,
+                  :cpu_set, :cpu_shares, :memory, :tty, :attach_stdin, :registry_id,
                   :attach_stdout, :attach_stderr, :tag, :uuid, :environment_variables_attributes
 
   def parametrize
     { 'name'  => name, # key has to be lower case to be picked up by the Docker API
-      'Image' => tag.tag.blank? ? image.image_id : "#{image.image_id}:#{tag.tag}",
+      'Image' => tag.blank? ? repository_name : "#{repository_name}:#{tag}",
       'Tty'          => tty,                    'Memory'       => memory,
       'Entrypoint'   => entrypoint.try(:split), 'Cmd'          => command.try(:split),
       'AttachStdout' => attach_stdout,          'AttachStdin'  => attach_stdin,

app/models/docker_image.rb
@@ -1,12 +1,0 @@
-class DockerImage < ActiveRecord::Base
-  has_many :tags, :class_name => 'DockerTag', :foreign_key => 'docker_image_id',
-                  :dependent  => :destroy
-  has_many :containers
-  has_many :docker_image_docker_registries
-  has_many :registries, :class_name => 'DockerRegistry', :uniq => true,
-           :through => :docker_image_docker_registries
-
-  attr_accessible :image_id, :size
-
-  validates :image_id, :presence => true, :uniqueness => true
-end

app/models/docker_image_docker_registry.rb
@@ -1,4 +1,0 @@
-class DockerImageDockerRegistry < ActiveRecord::Base
-  belongs_to :image, :class_name => DockerImage
-  belongs_to :registry, :class_name => DockerRegistry
-end

app/models/docker_registry.rb
@@ -2,9 +2,7 @@ class DockerRegistry < ActiveRecord::Base
   include Authorizable
   include Taxonomix
 
-  has_many :docker_image_docker_registries
-  has_many :images, :class_name => 'DockerImage',
-           :through => :docker_image_docker_registries, :uniq => true
+  has_many :containers, :foreign_key => "registry_id", :dependent => :destroy
 
   scoped_search :on => :name, :complete_value => true
   scoped_search :on => :url

parthaa/foreman-docker

commit 92618bcd8ff5c852c8c85dc23821ce0c6613d594

Fixes #8772 - Fixing external registry refactor blooper
Basically commit c57dae5641a637761fb51058ad2607c3a9db8ce1 broke the
external registry selection page for a new container
The attached code fixes it.

app/assets/javascripts/foreman_docker/image_step.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
   });
 
   $('#hub_tab').click( function() {
-      $('#repository_registry_id').val('');
+      $('#container_registry_id').val('');
   });
 });
 
@@ -52,7 +52,7 @@ function setAutocompleteTags() {
   tag.addClass('tags-autocomplete-loading');
   tag.val('');
   var source = [];
-  $.getJSON( tag.data("url"), { search: $('#search').val(), registry_id: $('#repository_registry_id').val() },
+  $.getJSON( tag.data("url"), { search: $('#search').val(), registry_id: $('#container_registry_id').val() },
       function(data) {
         $('#searching_spinner').hide();
         tag.removeClass('tags-autocomplete-loading');
@@ -72,7 +72,7 @@ function searchRepo(item) {
     type:'get',
     dataType:'text',
     url: $(item).attr('data-url'),
-    data: { search: $('#search').val(), registry_id: $('#repository_registry_id').val() },
+    data: { search: $('#search').val(), registry_id: $('#container_registry_id').val() },
     success: function (result) {
       $('#repository_search_results').html(result);
     },